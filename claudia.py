1aac3b7ee8896ae94d90c0a8eccaacdc83198797
Claudia-Arely-Perez-Mayo <84484574+Claudia-Arely-Perez-Mayo@users.noreply.github.com>
2021-06-15 16:35:31 -0500

    Update claudia.py
    Cambios

--- claudia.py
+++ claudia.py
@@ -5,5 +5,17 @@ app = FastAPI ()
 @app.get("/")
 def home():
 	return {"Claudia Arely"}
+
+Datos = {
+	1: {
+		"nombre": "Claudia",
+		"matrícula":"091810190",
+		"edad": "20"
+	}
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+	return Datos[item_id]
 	
 
